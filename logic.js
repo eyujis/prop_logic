ce36a65aaacedfc65cd692e196bd3e4daa7d4389
eyujisgit config --global user.name eyujis <eyujis@gmail.com>
2019-10-21 12:40:41 -0300

    problemas encontrados para sentence como parametro

--- logic.js
+++ logic.js
@@ -2,7 +2,6 @@ const Node = require('./tree_class.js');
 
 function reason (KB, x) {
 
-  console.log(x)
   for (i=0; i<KB.length; i++)  {
 
     //trivial
@@ -14,11 +13,11 @@ function reason (KB, x) {
     if (x.content=='NOT') {
 
       if(reason(KB, x.child)) {
-        console.log('False by not in query')
+        console.log('False by negation in query')
         return false
       }
       else {
-        console.log('True by not in query')
+        console.log('True by negation in query')
         return true
       }
     }
@@ -27,36 +26,38 @@ function reason (KB, x) {
     if (x.content = 'NOT')  {
 
       if(reason(KB[i].child,x))  {
+        console.log('False by negation in Knowledge base')
         return false
       } else {
         return true
+        console.log('True by negation in Knowledge base')
       }
     }
 
+    //conjunction introduction
     if (x.content=='OR' && (reason(KB, x.left)  || reason(KB,x.right))) {
-      console.log('True by Consjunction Introduction')
+      console.log('True by Conjunction Introduction')
       return true
     }
 
   }
-  console.log('END')
+  console.log('FALSE')
   return false
 }
 
 
-//push test with not in sentense
+push test with not in sentense
 
 var tree = [];
 tree.push(new Node('NOT', 'unary'));
 tree[0].child.push(new Node('a', 'literal'))
 var query = new Node ('a','literal');
-callback(console.log(query))
-//reason(tree, query);
+reason(tree, query);
 
 
 
 //
-// //disjunction introduction with double negation in query (OK)
+//disjunction introduction with double negation in query (OK)
 // var tree = [];
 // tree[0] = new Node('a', 'literal');
 // var query = [];
@@ -65,7 +66,7 @@ callback(console.log(query))
 // query[0].right[0] = new Node('NOT', 'unary');
 // query[0].right[0].child[0] = new Node('NOT', 'unary');
 // query[0].right[0].child[0].child[0] = new Node('a', 'literal');
-
+// console.log(reason(tree, query))
 
 
 // //disjunction introduction (OK)
